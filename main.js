d8dfde5f804d08112124d2cccf52c300bbca9bea
NolanThrondset2022 <816059@mystma.org>
2022-04-05 12:28:54 -0500

    more js stuff

--- main.js
+++ main.js
@@ -1,1 +1,73 @@
-// JavaScript Document
+function one() {
+	document.getElementById("one").innerHTML = "Now";
+	document.getElementById("one").style.backgroundColor = "Red";
+}
+document.getElementById("but1").addEventListener("click", one);
+
+
+function two() {
+	document.getElementById("one").innerHTML = "The";
+	document.getElementById("one").style.backgroundColor = "blue";
+}
+document.getElementById("but2").addEventListener("click", two);
+
+
+function three() {
+	document.getElementById("one").innerHTML = "Colors";
+	document.getElementById("one").style.backgroundColor = "green";
+}
+document.getElementById("but3").addEventListener("click", three);
+
+
+function four() {
+	document.getElementById("one").innerHTML = "Have";
+	document.getElementById("one").style.backgroundColor = "orange";
+}
+document.getElementById("but4").addEventListener("click", four);
+
+
+function five() {
+	document.getElementById("one").innerHTML = "All";
+	document.getElementById("one").style.backgroundColor = "pink";
+}
+document.getElementById("but5").addEventListener("click", five);
+
+
+function six() {
+	document.getElementById("one").innerHTML = "Changed";
+	document.getElementById("one").style.backgroundColor = "yellow";
+}
+document.getElementById("but6").addEventListener("click", six);
+
+
+function seven() {
+	document.getElementById("one").innerHTML = "To";
+	document.getElementById("one").style.backgroundColor = "teal";
+}
+document.getElementById("but7").addEventListener("click", seven);
+
+
+function eight() {
+	document.getElementById("one").innerHTML = "different";
+	document.getElementById("one").style.backgroundColor = "grey";
+}
+document.getElementById("but8").addEventListener("click", eight);
+
+
+function nine() {
+	document.getElementById("one").innerHTML = "Ones";
+	document.getElementById("one").style.backgroundColor = "purple";
+}
+document.getElementById("but9").addEventListener("click", nine);
+
+
+function ten() {
+	document.getElementById("one").innerHTML = "Except this one";
+	document.getElementById("one").style.backgroundColor = "white";
+}
+document.getElementById("but10").addEventListener("click", ten);
+
+
+
+
+
