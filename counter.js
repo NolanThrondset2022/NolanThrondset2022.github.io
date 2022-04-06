1cad35ae54fa4404cfed3edc36045bae925d12ab
NolanThrondset2022 <816059@mystma.org>
2022-04-06 12:14:05 -0500

    added more buttons

--- counter.js
+++ counter.js
@@ -1,5 +1,19 @@
 var count = 0;
 
+function divideTen() {
+	count = count / 10;
+	document.getElementById("counter").innerHTML = count;
+}
+
+function divideTwo() {
+	count = count / 2;
+	document.getElementById("counter").innerHTML = count;
+}
+function minusFive() {
+	count = count - 5;
+	document.getElementById("counter").innerHTML = count;
+}
+
 function minus() {
 	count = count - 1;
 	document.getElementById("counter").innerHTML = count;
@@ -9,7 +23,23 @@ function plus() {
 	count = count + 1;
 	document.getElementById("counter").innerHTML = count;
 }
-
+function plusFive() {
+	count = count + 5;
+	document.getElementById("counter").innerHTML = count;
+}
+function timesTwo() {
+	count = count * 2;
+	document.getElementById("counter").innerHTML = count;
+}
+function timesTen() {
+	count = count * 10;
+	document.getElementById("counter").innerHTML = count;
+}
 document.getElementById("Minus").addEventListener("click", minus);
 document.getElementById("Plus").addEventListener("click", plus);
-
+document.getElementById("PlusFive").addEventListener("click", plusFive);
+document.getElementById("MinusFive").addEventListener("click", minusFive);
+document.getElementById("DividedTwo").addEventListener("click", divideTwo);
+document.getElementById("DividedTen").addEventListener("click", divideTen);
+document.getElementById("TimesTwo").addEventListener("click", timesTwo);
+document.getElementById("TimesTen").addEventListener("click", timesTen);
