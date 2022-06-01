8914961cf1756b3a64d4ebfb26982be2798e4fcc
NolanThrondset2022 <816059@mystma.org>
2022-06-01 11:40:31 -0500

    A whole lot

---
+++ FINAL.js
@@ -0,0 +1,115 @@
+var b1 = document.getElementById("port");
+var b2 = document.getElementById("tree");
+var b3 = document.getElementById("pumpkin");
+var b4 = document.getElementById("hiring");
+var b5 = document.getElementById("car");
+var b6 = document.getElementById("fruit");
+var b7 = document.getElementById("hand");
+var b8 = document.getElementById("lights");
+var b9 = document.getElementById("king");
+var b10 = document.getElementById("shadow");
+var b11 = document.getElementById("fire");
+var b12 = document.getElementById("giant");
+var b13 = document.getElementById("big");
+var b14 = document.getElementById("wide");
+var b15 = document.getElementById("large");
+var b16 = document.getElementById("into");
+var b17 = document.getElementById("four");
+var b18 = document.getElementById("times");
+var b19 = document.getElementById("eight");
+var b20 = document.getElementById("two");
+var b21 = document.getElementById("dragon");
+var b22 = document.getElementById("win");
+var image = document.getElementById("image");
+
+
+function changePORT() {
+	image.src = "EldenIMG/b1.jpg";
+}
+function changeTREE() {
+	image.src = "EldenIMG/b2.jpg";
+}
+function changePUMP() {
+	image.src = "EldenIMG/b3.jpg";
+}
+function changeHIR() {
+	image.src = "EldenIMG/b4.jpg";
+}
+function changeCAR() {
+	image.src = "EldenIMG/b5.jpg";
+}
+function changeFRU() {
+	image.src = "EldenIMG/b6.jpg";
+}
+function changeHAND() {
+	image.src = "EldenIMG/b7.jpg";
+}
+function changeLIGH() {
+	image.src = "EldenIMG/b8.jpg";
+}
+function changeKING() {
+	image.src = "EldenIMG/b9.jpg";
+}
+function changeSHAD() {
+	image.src = "EldenIMG/b10.jpg";
+}
+function changeFIR() {
+	image.src = "EldenIMG/b11.jpg";
+}
+function changeGIA() {
+	image.src = "EldenIMG/b12.jpg";
+}
+function changeBIG() {
+	image.src = "EldenIMG/b13.jpg";
+}
+function changeWID() {
+	image.src = "EldenIMG/b14.jpg";
+}
+function changeLAR() {
+	image.src = "EldenIMG/b15.jpg";
+}
+function changeINT() {
+	image.src = "EldenIMG/b16.jpg";
+}
+function changeFOU() {
+	image.src = "EldenIMG/b17.jpg";
+}
+function changeTIM() {
+	image.src = "EldenIMG/b18.jpeg";
+}
+function changeEIG() {
+	image.src = "EldenIMG/b19.jpg";
+}
+function changeTWO() {
+	image.src = "EldenIMG/b20.jpg";
+}
+function changeDRA() {
+	image.src = "EldenIMG/b21.jpg";
+}
+function changeWIN() {
+	image.src = "EldenIMG/b22.jpg";
+}
+
+
+b1.addEventListener("click", changePORT);
+b2.addEventListener("click", changeTREE);
+b3.addEventListener("click", changePUMP);
+b4.addEventListener("click", changeHIR);
+b5.addEventListener("click", changeCAR);
+b6.addEventListener("click", changeFRU);
+b7.addEventListener("click", changeHAND);
+b8.addEventListener("click", changeLIGH);
+b9.addEventListener("click", changeKING);
+b10.addEventListener("click", changeSHAD);
+b11.addEventListener("click", changeFIR);
+b12.addEventListener("click", changeGIA);
+b13.addEventListener("click", changeBIG);
+b14.addEventListener("click", changeWID);
+b15.addEventListener("click", changeLAR);
+b16.addEventListener("click", changeINT);
+b17.addEventListener("click", changeFOU);
+b18.addEventListener("click", changeTIM);
+b19.addEventListener("click", changeEIG);
+b20.addEventListener("click", changeTWO);
+b21.addEventListener("click", changeDRA);
+b22.addEventListener("click", changeWIN);
